6ad8111e780039ba614e8a108b7509e118cd3b90
Hao <jatc1024@gmail.com>
2021-02-05 16:25:00 +0700

    Read using scanpy instead of manually doing it with h5py

--- pyBCS/scanpy2bcs.py
+++ pyBCS/scanpy2bcs.py
@@ -9,6 +9,7 @@ import uuid
 import time
 import shutil
 import zipfile
+from pandas.api.types import is_numeric_dtype
 
 def copy_dataset(source, dest, name, chunk_size=16*1024):
     dataset = dest.create_dataset(name, shape=source[name].shape, dtype=source[name].dtype)
@@ -27,41 +28,80 @@ def generate_uuid(remove_hyphen=True):
         res = res.replace("-", "")
     return res
 
-def write_matrix(source_hdf5, dest_hdf5):
+def get_barcodes(scanpy_obj):
+    return scanpy_obj.obs_names
+
+def get_features(scanpy_obj):
+    return scanpy_obj.var_names
+
+def get_raw_features(scanpy_obj):
+    return scanpy_obj.raw.var.index
+
+def get_raw_data(scanpy_obj):
+    try:
+        M = scanpy_obj.raw.X[:][:].tocsr()
+        barcodes = get_barcodes(scanpy_obj)
+        features = get_raw_features(scanpy_obj)
+    except:
+        if "counts" in scanpy_obj.layers:
+            raw_key = "counts"
+        elif "raw" in scanpy_obj.layers:
+            raw_key = "raw"
+        else:
+            raise Exception("Cannot find raw data")
+        M = scanpy_obj.layers[raw_key].tocsr()
+        barcodes = get_barcodes(scanpy_obj)
+        features = get_features(scanpy_obj)
+    return M, barcodes, features
+
+def get_normalized_data(scanpy_obj, raw_data):
+    M = scanpy_obj.X[:][:].tocsc()
+    if M.shape == raw_data.shape:
+        return M
+    else:
+        return raw_data
+
+def encode_strings(strings, encode_format="utf8"):
+    return [x.encode(encode_format) for x in strings]
+
+def write_matrix(scanpy_obj, dest_hdf5):
+    raw_M, barcodes, features = get_raw_data(scanpy_obj)
     print("--->Writing group \"bioturing\"")
     bioturing_group = dest_hdf5.create_group("bioturing")
-    barcodes = [x.encode("utf8") for x in source_hdf5["obs"]["index"][:]]
-    bioturing_group.create_dataset("barcodes", data=barcodes)
-    features = [x.encode("utf8") for x in source_hdf5["var"]["index"][:]]
-    bioturing_group.create_dataset("features", data=features)
-    M = scipy.sparse.csr_matrix((source_hdf5["layers"]["counts"]["data"],
-                                 source_hdf5["layers"]["counts"]["indices"],
-                                 source_hdf5["layers"]["counts"]["indptr"]),
-                                shape=(len(features), len(barcodes)))
-    M = M.tocsc()
-    bioturing_group.create_dataset("data", data=M.data)
-    bioturing_group.create_dataset("indices", data=M.indices)
-    bioturing_group.create_dataset("indptr", data=M.indptr)
+    bioturing_group.create_dataset("barcodes",
+                                    data=encode_strings(barcodes))
+    bioturing_group.create_dataset("features",
+                                    data=encode_strings(features))
+    bioturing_group.create_dataset("data", data=raw_M.data)
+    bioturing_group.create_dataset("indices", data=raw_M.indices)
+    bioturing_group.create_dataset("indptr", data=raw_M.indptr)
     bioturing_group.create_dataset("feature_type", data=["RNA".encode("utf8")] * len(features))
     bioturing_group.create_dataset("shape", data=[len(features), len(barcodes)])
 
     print("--->Writing group \"countsT\"")
+    raw_M_T = raw_M.tocsc()
     countsT_group = dest_hdf5.create_group("countsT")
-    countsT_group.create_dataset("barcodes", data=features)
-    countsT_group.create_dataset("features", data=barcodes)
-    copy_dataset(source_hdf5["layers"]["counts"], countsT_group, "indptr")
-    copy_dataset(source_hdf5["layers"]["counts"], countsT_group, "indices")
-    copy_dataset(source_hdf5["layers"]["counts"], countsT_group, "data")
+    countsT_group.create_dataset("barcodes",
+                                    data=encode_strings(features))
+    countsT_group.create_dataset("features",
+                                    data=encode_strings(barcodes))
+    countsT_group.create_dataset("data", data=raw_M_T.data)
+    countsT_group.create_dataset("indices", data=raw_M_T.indices)
+    countsT_group.create_dataset("indptr", data=raw_M_T.indptr)
     countsT_group.create_dataset("shape", data=[len(barcodes), len(features)])
 
     print("--->Writing group \"normalizedT\"")
+    norm_M = get_normalized_data(scanpy_obj, raw_M)
     normalizedT_group = dest_hdf5.create_group("normalizedT")
-    normalizedT_group.create_dataset("barcodes", data=features)
-    normalizedT_group.create_dataset("features", data=barcodes)
-    copy_dataset(source_hdf5["X"], normalizedT_group, "indptr")
-    copy_dataset(source_hdf5["X"], normalizedT_group, "indices")
-    copy_dataset(source_hdf5["X"], normalizedT_group, "data")
+    normalizedT_group.create_dataset("barcodes",
+                                    data=encode_strings(features))
+    normalizedT_group.create_dataset("features",
+                                    data=encode_strings(barcodes))
+    normalizedT_group.create_dataset("data", data=norm_M.data)
+    normalizedT_group.create_dataset("indices", data=norm_M.indices)
+    normalizedT_group.create_dataset("indptr", data=norm_M.indptr)
     normalizedT_group.create_dataset("shape", data=[len(barcodes), len(features)])
+    return barcodes, features
 
 def generate_history_object():
     return {
@@ -90,44 +130,44 @@ def convert_to_float(a):
     except:
         return None
 
-def write_metadata(source_hdf5, dest, zobj):
+def write_metadata(scanpy_obj, dest, zobj):
     print("Writing main/metadata/metalist.json")
+    metadata = scanpy_obj.obs.copy()
+    for metaname in metadata.columns:
+        try:
+            metadata[metaname] = pd.to_numeric(metadata[metaname], downcast="float")
+        except:
+            print("--->Cannot convert %s to numeric, treating as categorical" % metaname)
+
     content = {}
-    categories = source_hdf5["obs"]["__categories"]
-    n_cells = len(source_hdf5["obs"]["index"])
     all_clusters = {}
-    for metaname in source_hdf5["obs"]:
-        if metaname in ["__categories", "index"]:
-            continue
-        if len(np.unique(categories[metaname])) == n_cells:
-            print("--->Ignore metadata %s" % metaname)
-            continue
+    numeric_meta = metadata.select_dtypes(include=["number"]).columns
+    category_meta = metadata.select_dtypes(include=["category"]).columns
+    for metaname in metadata.columns:
         uid = generate_uuid()
-        if metaname in categories:
-            if is_all_numeric(categories[metaname][:]):
-                values = convert_to_int(categories[metaname][:])
-                if values is None:
-                    values = convert_to_float(categories[metaname][:])
-                values = np.array(values)
-                all_clusters[uid] = values[source_hdf5["obs"][metaname][:]]
-            else:
-                all_clusters[uid] = source_hdf5["obs"][metaname][:] + 1
-        else:
-            all_clusters[uid] = source_hdf5["obs"][metaname][:]
 
-        all_clusters[uid] = [x.item() for x in all_clusters[uid]]
-
-        if metaname in categories and not is_all_numeric(categories[metaname][:]):
-            names = ["Unassigned"] + list(categories[metaname][:])
-            lengths = np.array([0] * len(names))
-            ids, counts = np.unique(source_hdf5["obs"][metaname][:], return_counts=True)
-            lengths[ids + 1] = counts
-            lengths = [x.item() for x in lengths]
-            _type = "category"
-        else:
+        if metaname in numeric_meta:
+            all_clusters[uid] = list(metadata[metaname])
             lengths = 0
             names = "NaN"
             _type = "numeric"
+        elif metaname in category_meta:
+            value_to_index = {}
+            print(metaname)
+            for x, y in enumerate(metadata[metaname].cat.categories):
+                value_to_index[y] = x
+            all_clusters[uid] = [value_to_index[x] + 1 for x in metadata[metaname]]
+            index, counts = np.unique(all_clusters[uid], return_counts = True)
+            lengths = np.array([0] * (len(index) + 1))
+            lengths[index] = counts
+            lengths = [x.item() for x in lengths]
+            _type = "category"
+            names = ["Unassigned"] + list(enumerate(metadata[metaname].cat.categories))
+        else:
+            print("--->\"%s\" is not numeric or categorical, ignoring" % metaname)
+            continue
+
+
         content[uid] = {
             "id":uid,
             "name":metaname if metaname != "seurat_clusters" else "Graph-based clusters",
@@ -139,6 +179,7 @@ def write_metadata(source_hdf5, dest, zobj):
 
     graph_based_history = generate_history_object()
     graph_based_history["hash_id"] = "graph_based"
+    n_cells = scanpy_obj.n_obs
     content["graph_based"] = {
         "id":"graph_based",
         "name":"Graph-based clusters",
@@ -169,41 +210,38 @@ def write_metadata(source_hdf5, dest, zobj):
         with zobj.open(dest + ("/main/metadata/%s.json" % uid), "w") as z:
             z.write(json.dumps(obj).encode("utf8"))
 
-def write_main_folder(source_hdf5, dest, zobj):
+def write_main_folder(scanpy_obj, dest, zobj):
     print("Writing main/matrix.hdf5", flush=True)
     tmp_matrix = "." + str(uuid.uuid4())
     with h5py.File(tmp_matrix, "w") as dest_hdf5:
-        write_matrix(source_hdf5, dest_hdf5)
+        barcodes, features = write_matrix(scanpy_obj, dest_hdf5)
+    print("--->Writing to zip", flush=True)
     zobj.write(tmp_matrix, dest + "/main/matrix.hdf5")
     os.remove(tmp_matrix)
 
     print("Writing main/barcodes.tsv", flush=True)
-    barcodes = "\n".join(source_hdf5["obs"]["index"][:]).encode("utf8")
     with zobj.open(dest + "/main/barcodes.tsv", "w") as z:
-        z.write(barcodes)
+        z.write("\n".join(barcodes).encode("utf8"))
 
     print("Writing main/genes.tsv", flush=True)
-    features = "\n".join(source_hdf5["var"]["index"][:]).encode("utf8")
     with zobj.open(dest + "/main/genes.tsv", "w") as z:
-        z.write(features)
+        z.write("\n".join(features).encode("utf8"))
 
     print("Writing main/gene_gallery.json", flush=True)
     obj = {"gene":{"nameArr":[],"geneIDArr":[],"hashID":[],"featureType":"gene"},"version":1,"protein":{"nameArr":[],"geneIDArr":[],"hashID":[],"featureType":"protein"}}
     with zobj.open(dest + "/main/gene_gallery.json", "w") as z:
         z.write(json.dumps(obj).encode("utf8"))
 
-def write_dimred(source_hdf5, dest, zobj):
+def write_dimred(scanpy_obj, dest, zobj):
     print("Writing dimred")
     data = {}
     default_dimred = None
-    for dimred in source_hdf5["obsm"]:
-        try:
-            x = source_hdf5["obsm"][dimred].shape
-        except Exception as e:
-            print("--->Ignoring %s" % dimred)
+    for dimred in scanpy_obj.obsm:
+        if isinstance(scanpy_obj.obsm[dimred], np.ndarray) == False:
+            print("--->%s is not a numpy.ndarray, ignoring" % dimred)
             continue
         print("--->Writing %s" % dimred)
-        matrix = source_hdf5["obsm"][dimred][:][:]
+        matrix = scanpy_obj.obsm[dimred]
         if matrix.shape[1] > 3:
             print("--->%s has more than 3 dimensions, using only the first 3 of them" % dimred)
             matrix = matrix[:, 0:3]
@@ -242,7 +280,7 @@ def write_dimred(source_hdf5, dest, zobj):
         z.write(json.dumps(meta).encode("utf8"))
 
 
-def write_runinfo(source_hdf5, dest, study_id, zobj):
+def write_runinfo(scanpy_obj, dest, study_id, zobj):
     print("Writing run_info.json", flush=True)
     runinfo_history = generate_history_object()
     runinfo_history["hash_id"] = study_id
@@ -251,7 +289,7 @@ def write_runinfo(source_hdf5, dest, study_id, zobj):
         "species":"human",
         "hash_id":study_id,
         "version":16,
-        "n_cell":len(source_hdf5["obs"]["index"]),
+        "n_cell":scanpy_obj.n_obs,
         "modified_date":date,
         "created_date":date,
         "addon":"SingleCell",
@@ -284,16 +322,17 @@ def check_format(source):
                 raise type(e)("Error when checking %s: %s" % (p, str(e)))
 
 def format_data(source, output_name, overwrite=False):
-    check_format(source)
+    #check_format(source)
 
+    scanpy_obj = scanpy.read_h5ad(source, "r")
     zobj = zipfile.ZipFile(output_name, "w")
     study_id = generate_uuid(remove_hyphen=False)
     dest = study_id
     with h5py.File(source, "r") as s:
-        write_main_folder(s, dest, zobj)
-        write_metadata(s, dest, zobj)
-        write_dimred(s, dest, zobj)
-        write_runinfo(s, dest, study_id, zobj)
+        write_main_folder(scanpy_obj, dest, zobj)
+        write_metadata(scanpy_obj, dest, zobj)
+        write_dimred(scanpy_obj, dest, zobj)
+        write_runinfo(scanpy_obj, dest, study_id, zobj)
 
     return output_name
 
