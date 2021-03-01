724c98a4e7cd6022ad906d12a01f103274e6f054
hao <jatc1024@gmail.com>
2021-03-01 06:00:43 -0500

    Add additional log

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyBCS-bioturing",
-    version="0.1.0",
+    version="0.1.1",
     author="BioTuring",
     author_email="support@bioturing.com",
     description="Create BioTuring Compressed Study (bcs) file",
